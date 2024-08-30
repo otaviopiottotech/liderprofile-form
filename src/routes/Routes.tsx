d536956690a9b7b3b6f9ba763b9b264dab398162
otaviopiotto <otavioapiotto@hotmail.com>
2024-08-30 11:28:02 -0300

    possibility to share

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import QuizScreen from "../pages/quiz";
 import Ques from "../pages/questionario";
 import ReportScreen from "../pages/reports";
+import HomeScreen from "../pages/home";
 
 const AppRoutes = () => {
   return <UserRoute />;
@@ -11,7 +12,8 @@ const UserRoute = () => {
   return (
     <Routes>
       <Route path="*" element={<Navigate replace to="/" />} />
-      <Route path="/" element={<QuizScreen />} />
+      <Route path="/" element={<HomeScreen />} />
+      <Route path="/criar-questionario" element={<QuizScreen />} />
       <Route path="/questinoario" element={<Ques />} />
       <Route path="/relatorio" element={<ReportScreen />} />
     </Routes>
